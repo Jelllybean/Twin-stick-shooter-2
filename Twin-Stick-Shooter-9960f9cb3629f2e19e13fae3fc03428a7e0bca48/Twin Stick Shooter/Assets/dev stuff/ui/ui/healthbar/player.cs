d01ffe8c5890eed4ad7a63f08e34ba5a3c263fc4
RayTero <525513@student.glu.nl>
2018-05-15 09:33:44 +0200

    yeh
    hopelijk werkt dit

---
+++ Twin-Stick-Shooter-9960f9cb3629f2e19e13fae3fc03428a7e0bca48/Twin Stick Shooter/Assets/dev stuff/ui/ui/healthbar/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // player health 
+    [SerializeField]
+    private Stats health; 
+
+    // Use this for initialization
+    void Start ()
+    {
+        health.initialize();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            health.CurrentHealth -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            health.CurrentHealth += 10;
+        }
+    }
+}
