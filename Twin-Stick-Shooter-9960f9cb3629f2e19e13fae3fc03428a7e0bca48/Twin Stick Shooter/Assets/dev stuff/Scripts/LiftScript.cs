f9d3210479ad76070d10e48fc2edbb6eb48088bd
Jelle van der Schaaf <jellesimonvds@gmail.com>
2018-06-24 13:06:27 +0200

    lift intro afgemaakt + lights

--- Twin-Stick-Shooter-9960f9cb3629f2e19e13fae3fc03428a7e0bca48/Twin Stick Shooter/Assets/dev stuff/Scripts/LiftScript.cs
+++ Twin-Stick-Shooter-9960f9cb3629f2e19e13fae3fc03428a7e0bca48/Twin Stick Shooter/Assets/dev stuff/Scripts/LiftScript.cs
@@ -10,6 +10,8 @@ public class LiftScript : MonoBehaviour {
     private GameObject lift;
     [SerializeField]
     private AudioSource m_LiftMusic;
+    [SerializeField]
+    private PlayerController m_move;
 
     [SerializeField]
     private Camera MainCamera;
@@ -20,12 +22,16 @@ public class LiftScript : MonoBehaviour {
     bool OnLift;
     private void OnCollisionStay(Collision other)
     {
-        if (other.collider.tag == "Player")
+
+        if (OnLift == true)
         {
-            Speler.transform.SetParent(transform);
-            ActivateLift();
-            MainCamera.enabled = false;
-            LiftCamera.enabled = true;
+            if (other.collider.tag == "Player")
+            {
+                Speler.transform.SetParent(transform);
+                MainCamera.enabled = false;
+                LiftCamera.enabled = true;
+                transform.position -= speed;
+            }
         }
     }
     private void OnCollisionExit(Collision other)
@@ -36,30 +42,28 @@ public class LiftScript : MonoBehaviour {
             OnLift = false;
             MainCamera.enabled = true;
             LiftCamera.enabled = false;
+            m_move.enabled = true;
             m_LiftMusic.Stop();
+            OnLift = false;
         }
     }
-    // Use this for initialization
     void Start ()
     {
+        m_move.enabled = false;
         speed.y = 0.1f;
-	}
+        OnLift = true;
+        m_LiftMusic.Play();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (OnLift == true)
-        {
-            transform.position -= speed;
-        }
+
     }
 
     public void ActivateLift()
     {
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            OnLift = true;
-            m_LiftMusic.Play();
-        }
+
+
     }
 }
